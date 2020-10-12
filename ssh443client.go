1e913c458f877bccb6d450ac12b6d7e7c0d8318a
Louis Buchbinder <git@louisbuchbinder.com>
2020-10-11 23:44:57 -0400

    support tls

--- ssh443client.go
+++ ssh443client.go
@@ -1,48 +1,93 @@
 package main
 import "bufio"
+import "crypto/tls"
 import "encoding/base64"
+import "errors"
+import "flag"
 import "fmt"
 import "io"
 import "net"
 import "os"
+import "strings"
 
-func main() {
-  args := os.Args
+type args struct {
+  address string
+  auth string
+  http bool
+  proxy string
+}
 
-  if (len(args) != 4) {
-    fmt.Println("ERROR: Usage `./ssh443client <auth> <proxyHost>:<proxyPort> <host>:<port>`")
-    os.Exit(1)
+func cliArgs() args {
+  address := flag.String("address", "", "<host>:<port>")
+  auth := flag.String("auth", "", "<user>:<pass>")
+  proxy := flag.String("proxy", "", "<host>:<port>")
+  http := flag.Bool("http", false, "use http")
+
+  flag.Parse()
+
+  if (*address == "" || *auth == "" || *proxy == "") {
+    flag.Usage()
+    os.Exit(2)
   }
 
-  auth := base64.StdEncoding.EncodeToString([]byte(args[1]))
-  proxyAddress := args[2]
-  address := args[3]
+  return args{
+    address: *address,
+    auth: *auth,
+    proxy: *proxy,
+    http: *http,
+  }
+}
 
-  // TODO: add crypto/tls
-  conn, err := net.Dial("tcp", proxyAddress)
-
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
+func getConn(proxy string, ssl bool) (net.Conn) {
+  var conn net.Conn
+  var err error
+  if (ssl) {
+    conn, err = tls.Dial("tcp", proxy, &tls.Config{})
+  } else {
+    conn, err = net.Dial("tcp", proxy)
   }
 
+  if (err != nil) {
+    panic(err)
+  }
+
+  return conn
+}
+
+func sendHeaders(conn net.Conn, auth string, address string, proxy string) {
   fmt.Fprintf(conn, "CONNECT " + address + " HTTP/1.1\r\n")
   fmt.Fprintf(conn, "Proxy-Authorization: Basic " + auth + "\r\n")
+  fmt.Fprintf(conn, "Host: " + proxy + "\r\n")
   fmt.Fprintf(conn, "\r\n")
+}
+
+func awaitConnectionReady(conn net.Conn) {
   status, err := bufio.NewReader(conn).ReadString('\n')
 
   if err == io.EOF {
-    fmt.Println("ERROR: connection closed by remote host")
-    os.Exit(0)
+    panic(errors.New("ERROR: connection closed by remote host"))
   }
   if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
+    panic(err)
   }
 
-  fmt.Println(status)
+  fields := strings.Fields(status)
+  if (len(fields) < 3 || fields[1] != "200") {
+    panic(errors.New("ERROR: proxy connection failure"))
+  }
+}
 
+func pipeStdIO(conn net.Conn) {
   defer conn.Close()
   go io.Copy(conn, os.Stdin)
   io.Copy(os.Stdout, conn)
+}
+
+func main() {
+  args := cliArgs()
+  auth := base64.StdEncoding.EncodeToString([]byte(args.auth))
+  conn := getConn(args.proxy, !args.http)
+  sendHeaders(conn, auth, args.address, args.proxy)
+  awaitConnectionReady(conn)
+  pipeStdIO(conn)
 }
